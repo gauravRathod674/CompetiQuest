9f556eb6838b490a0a732168144e65957bc21b40
gauravRathod674 <gauravrathod.imscit21@gmail.com>
2025-08-15 00:15:27 +0530

    Created base folder structure

--- Client/app/page.js
+++ Client/app/page.js
@@ -12,3 +12,12 @@ export default function Home() {
     </div>
   );
 }
+
+/*
+*IMP Instructions For Using GitHub*
+
+=> Sab apni feature-wise branch banao (e.g. frontend/homepage-ui, backend/api-integration). Kabhi direct main pe kaam mat karo.
+=> Kisi ka branch overwrite na karo, galti se bhi force push mat karo.
+=> Pull karne se pehle apne changes save & commit kar lo.
+=> Conflicts aane par bina soche samjhe resolve na karo, pehle team ko inform karo.
+*/
