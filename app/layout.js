e3677851ec6458e5a323cfdd2f5121710229ee1e
gauravRathod674 <gauravrathod.imscit21@gmail.com>
2025-08-24 15:29:18 +0530

    Save local changes before switching branch

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import { ThemeProvider } from "./context/ThemeContext"; 
+import { ThemeProvider } from "./context/ThemeContext";
+// import ChatBotProvider from "../components/ChatBotProvider"; 
 import "./globals.css";
 
 const geistSans = Geist({
@@ -30,6 +31,7 @@ export default function RootLayout({ children }) {
           <Navbar />
           {children}
           <Footer />
+          {/* <ChatBotProvider /> */}
         </ThemeProvider>
       </body>
     </html>
